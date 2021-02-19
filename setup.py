96d141a9c486bb74e41040b16f439ad5988cf58a
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-02-19 22:44:23 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cli',
+    version='1.0',
+    description='Command Line Interface',
+    license='MIT',
+    author='Ekaterina Ledeneva',
+    author_email='ekiuled@gmail.com',
+    packages=['cli']
+)
